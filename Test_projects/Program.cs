de594181850c8a084117e2c5dbc506050d4b74e0
Keegan <keegan.adams15@gmail.com>
2021-04-07 17:46:53 -0500

    first commit

---
+++ Test_projects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
