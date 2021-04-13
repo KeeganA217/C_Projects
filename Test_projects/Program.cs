5e8d593dc61a66ea14f49d52ad36367ada696fa0
Keegan <keegan.adams15@gmail.com>
2021-04-12 19:01:40 -0500

    new commit

--- Test_projects/Program.cs
+++ Test_projects/Program.cs
@@ -7,15 +7,16 @@ namespace Test_projects
         static void Main(string[] args)
         {
 
-            var total =  new int[50];
-            for(var i = 1; i <= 100; i++)
-            {
-                if(i % 3 == 0)
-                {
-                    total[i] = i;
-                    Console.WriteLine(total);
-                }
-            }
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Length:" + numbers.Length);
+
+            var index = Array.IndexOf(numbers, 4);
+            Console.WriteLine(index);
+
+            var test = Array.Clear(numbers, 2, 2);
+
+            Console.WriteLine(test);
 
         }
     }
